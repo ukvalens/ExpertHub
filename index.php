e495760061122b1fec37884d02c9dfd9f91f9479
UKWITEGETSE Valens <ukwitegetsev9@gmail.com>
2026-02-11 18:53:57 +0200

    Update About section with auto-sliding carousels and smooth scroll navigation

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>ExpertHub - Online Service Marketplace</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="assets/css/style.css?v=11" rel="stylesheet">
+    <link href="assets/css/style.css?v=12" rel="stylesheet">
 
 </head>
 <body>

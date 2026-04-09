75a90d3fa7985769807965d6e705aada2f6c9547
ukvalens <139754672+ukvalens@users.noreply.github.com>
2026-04-09 11:05:59 +0200

    Reduce footer size and improve responsiveness

--- includes/footer.php
+++ includes/footer.php
@@ -1,53 +1,50 @@
 <!-- Footer -->
 <footer class="bg-dark text-light py-3 mt-3">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-4 mb-4">
-                <h5><i class="fas fa-users-cog me-2"></i>ExpertHub</h5>
-                <p class="text-light">Your trusted marketplace for connecting with verified professionals and getting quality work delivered on time.</p>
-                <div class="d-flex gap-3">
+        <div class="row g-2">
+            <div class="col-lg-4 col-md-6 mb-2">
+                <h6 class="mb-1"><i class="fas fa-users-cog me-1"></i>ExpertHub</h6>
+                <p class="text-light small mb-1">Your trusted marketplace for connecting with verified professionals.</p>
+                <div class="d-flex gap-2">
                     <a href="https://www.facebook.com/profile.php?id=100071330835838" class="text-light"><i class="fab fa-facebook-f"></i></a>
                     <a href="https://x.com/UKWITEGETSEVal1" class="text-light"><i class="fab fa-twitter"></i></a>
                     <a href="https://www.linkedin.com/in/ukwitegetse-valens-b52773356/" class="text-light"><i class="fab fa-linkedin-in"></i></a>
                     <a href="https://www.instagram.com/ukwitegetsevalens/" class="text-light"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
-            <div class="col-lg-2 mb-4">
-                <h6>Services</h6>
-                <ul class="list-unstyled">
+            <div class="col-lg-2 col-md-6 col-6 mb-2">
+                <h6 class="mb-1">Services</h6>
+                <ul class="list-unstyled small mb-0">
                     <li><a href="#" class="text-light text-decoration-none">Web Development</a></li>
                     <li><a href="#" class="text-light text-decoration-none">ICT Service</a></li>
                     <li><a href="#" class="text-light text-decoration-none">Digital maintenance</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Applicatons service</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Applications service</a></li>
                 </ul>
             </div>
-            <div class="col-lg-2 mb-4">
-                <h6>Company</h6>
-                <ul class="list-unstyled">
+            <div class="col-lg-2 col-md-6 col-6 mb-2">
+                <h6 class="mb-1">Company</h6>
+                <ul class="list-unstyled small mb-0">
                     <li><a href="#about" class="text-light text-decoration-none">About Us</a></li>
                     <li><a href="#" class="text-light text-decoration-none">+250780468216</a></li>
                     <li><a href="#" class="text-light text-decoration-none">valens4607@gmail.com</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Valens</a></li>
                 </ul>
             </div>
-
-            <div class="col-lg-2 mb-4">
-                <h6>Support</h6>
-                <ul class="list-unstyled">
+            <div class="col-lg-2 col-md-6 col-6 mb-2">
+                <h6 class="mb-1">Support</h6>
+                <ul class="list-unstyled small mb-0">
                     <li><a href="#" class="text-light text-decoration-none">Hardware maintenance</a></li>
                     <li><a href="#" class="text-light text-decoration-none">Software development</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Applications</a></li>
                     <li><a href="#" class="text-light text-decoration-none">Networking</a></li>
                 </ul>
             </div>
         </div>
-        <hr class="my-4">
+        <hr class="my-2">
         <div class="row align-items-center">
             <div class="col-md-6">
-                <p class="mb-0">&copy; <?php echo date('Y'); ?> ExpertHub. All rights reserved.</p>
+                <p class="mb-0 small">&copy; <?php echo date('Y'); ?> ExpertHub. All rights reserved.</p>
             </div>
             <div class="col-md-6 text-md-end">
-                <p class="mb-0 text-light">Made with <i class="fas fa-heart text-danger"></i> in Rwanda</p>
+                <p class="mb-0 small">Made with <i class="fas fa-heart text-danger"></i> in Rwanda</p>
             </div>
         </div>
     </div>
